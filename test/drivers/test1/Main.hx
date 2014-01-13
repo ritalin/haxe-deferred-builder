e3dbe868c54215608e684331f56837a57f364f06
Ritalin <kazu.ktz.tamura@gmail.com>
2014-01-13 19:13:19 +0900

    Add test sample

--- test/drivers/test1/Main.hx
+++ test/drivers/test1/Main.hx
@@ -15,12 +15,18 @@ private class Target {
 	@:async
 	public function run(): Promise {
 		var n1 = @:yield callAsync(1);
+		var n2 = @:yield callAsync(n1);
 
-		@:yield callAsync(n1);
+		@:yield callAsync2(n2);
 	}
 
 	@:async
 	private function callAsync(n: Int) {
 		return @:yield (n * 10);
+	}
+
+	@:async
+	private function callAsync2(n: Int) {
+		return @:yield [n, n*2];
 	}
 }
